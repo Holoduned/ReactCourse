08b66f2ed4a112125c79f17f2ceff99ea9d405df
Spirina Anastasia <94117859+Holoduned@users.noreply.github.com>
2025-01-10 16:23:25 +0300

    add styles

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,22 +1,23 @@
 import {SafeAreaView, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {CustomIcon} from '../assets/components/icons/Icon.ts';
+import styles from '../stylesheets/MainStyle';
 
 export const MainScreen = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text style={{fontSize: 40,
+      <View style={styles.container}>
+        <Text style={{fontSize: 20,
           fontFamily: 'RubikBubbles-Regular'}}>Я должен быть красивым текстом
         </Text>
-        <Text style={{fontSize: 40}}>Я должен быть обычным текстом</Text>
+        <Text style={{fontSize: 20}}>Я должен быть обычным текстом</Text>
       </View>
-      <View>
-        <Text>Я обычная иконка</Text>
+      <View style={styles.container}>
+        <Text style={{fontSize: 20}}>Я обычная иконка</Text>
         <Icon name="gift" size={30} color="#900" />
       </View>
-      <View>
-        <Text>Я шрифтовая иконка</Text>
+      <View style={styles.container}>
+        <Text style={{fontSize: 20}}>Я шрифтовая иконка</Text>
         <CustomIcon name={'ic_fluent_search_24_regular'} size={24} color={'red'} />
         <CustomIcon name="ic_fluent_home_28_regular" color={'#666666'} size={28} />
       </View>
--- stylesheets/MainStyle.js
+++ stylesheets/MainStyle.js
@@ -1,1 +1,15 @@
+import { StyleSheet} from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginHorizontal: 10,
+    padding: 20,
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 10,
+  },
+});
+
+export default styles;
